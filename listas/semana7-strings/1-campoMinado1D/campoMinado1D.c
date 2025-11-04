c673e7acd55edb6c5c8e5e1a44a1a50f5356eb77
DenisRdSilva <den_you@hotmail.com>
2025-11-04 13:29:54 -0300

    primeira questão da lista de strings

--- listas/semana7-strings/1-campoMinado1D/campoMinado1D.c
+++ listas/semana7-strings/1-campoMinado1D/campoMinado1D.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
+#include <string.h> 
+
 int main() {
-    float altura;
-    int peso;
+    char palavra[20];
+    int indice = 0;
 
-    printf("Digite o peso em Kg: ");
-    scanf("%d", &peso);
+    char elemento[20];
+    printf("Digite x ou .: ");
+    scanf("%s", elemento);
 
-    printf("Digite a altura em m: ");
-    scanf("%f", &altura);
+    for (int i = 0; i < strlen(elemento) && indice < sizeof(palavra) - 1; i++) {
+        if (elemento[i] == 'x' || elemento[i] == '.') {
+            palavra[indice] = elemento[i];
+            indice++;
+        }
+    } // Preenche o array com 'x' e '.'
 
-    float doublealtura = altura*altura;
-    float imc = peso/doublealtura;
-    
-    printf("resultado é: %.2f", imc);
+    palavra[indice] = '\0'; // Finaliza a string
+
+    int checarIndice(int ind) {
+        if (palavra[ind] == 'x') {
+            return 1; // Achou uma bomba
+        }
+        return 0; // Indice válido
+    }
+
+    printf("Digite o indice: ");
+    scanf("%d", &indice);
+
+    if (checarIndice(indice)) {
+        printf("bum!");
+    } else {
+        int total = checarIndice(indice - 1);
+        total += checarIndice(indice + 1);
+        printf("%d\n", total);
+    }
+
     return 0;
 }
